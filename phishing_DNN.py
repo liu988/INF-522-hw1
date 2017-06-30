47f5abe0e1051f28302d73b6fa79ec803bb67963
liu988 <liuyifan110808@gmail.com>
2017-06-29 18:22:04 -0700

    Add files via upload

--- phishing_DNN.py
+++ phishing_DNN.py
@@ -1,9 +1,12 @@
+from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
+from tensorflow.contrib.learn.python.learn.metric_spec import MetricSpec
 
 import tensorflow as tf
 import numpy as np
 from sklearn.cross_validation import train_test_split
-
+tf.logging.set_verbosity(tf.logging.INFO)
 # Data sets
 PHISHING_DATASET = "phishing_dataset.csv"
 
@@ -13,7 +16,7 @@ phishing_set = tf.contrib.learn.datasets.base.load_csv_with_header(
         target_dtype=np.int,
         features_dtype=np.int)
 
-# prep phishing_dataset inputs, test_size:(train=0.4,test=0.6)
+# prep phishing_dataset inputs
 X_training, X_test, Y_training, Y_test = train_test_split(
             phishing_set.data,
             phishing_set.target,
@@ -30,20 +33,41 @@ for i in range(len(Y_test)):
 
 feature_columns = [tf.contrib.layers.real_valued_column("", dimension=9)]
 
-# build 3 layer DNN with 10, 20, 10 units respectively. hidden_units:(how many layers, number of neurons)
-mdir = "/tmp/model"
+validation_metrics = {
+      "accuracy": MetricSpec(
+                          metric_fn=tf.contrib.metrics.streaming_accuracy,
+                          prediction_key="classes"),
+      "recall": MetricSpec(
+                          metric_fn=tf.contrib.metrics.streaming_recall,
+                          prediction_key="classes"),
+      "precision": MetricSpec(
+                          metric_fn=tf.contrib.metrics.streaming_precision,
+                          prediction_key="classes")
+                        }
+validation_monitor = tf.contrib.learn.monitors.ValidationMonitor(
+      X_test,
+      Y_test,
+      every_n_steps=50,
+      metrics=validation_metrics,
+      early_stopping_metric="loss",
+      early_stopping_metric_minimize=True,
+      early_stopping_rounds=200)
+  
+# build 3 layer DNN with 10, 20, 10 units respectively.
+mdir = "/tmp/phish_model"
 classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
-                                              hidden_units=[10,20,10], 
-                                              n_classes=3,
-                                            model_dir=mdir)
+                                            hidden_units=[10,20,10],
+                                            n_classes=3,
+                                            model_dir=mdir,
+                                            config=tf.contrib.learn.RunConfig(save_checkpoints_secs=1))
 
 def get_train_inputs():
     x = tf.constant(X_training)
     y = tf.constant(Y_training)
     return x, y
 
-# DO THE TRAINING! steps:(how many times to repeat the whole trainign/testing process untill it converges)
-classifier.fit(input_fn=get_train_inputs, steps=2000)
+# DO THE TRAINING!
+classifier.fit(input_fn=get_train_inputs, steps=1000, monitors=[validation_monitor])
 
 # prep test inputs
 def get_test_inputs():
