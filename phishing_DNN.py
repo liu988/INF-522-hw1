6f910930d9630c4c00680df6f301df1b0149769a
liu988 <liuyifan110808@gmail.com>
2017-06-29 17:30:13 -0700

    Add files via upload

---
+++ phishing_DNN.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+from sklearn.cross_validation import train_test_split
+
+# Data sets
+PHISHING_DATASET = "phishing_dataset.csv"
+
+# load data
+phishing_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+        filename=PHISHING_DATASET,
+        target_dtype=np.int,
+        features_dtype=np.int)
+
+# prep phishing_dataset inputs
+X_training, X_test, Y_training, Y_test = train_test_split(
+            phishing_set.data,
+            phishing_set.target,
+            test_size=0.6,
+            random_state=4)
+
+# increase test inputs by 1: (-1,0,1)->(0,1,2)
+for i in range(len(Y_training)):
+    Y_training[i] +=1
+    
+for i in range(len(Y_test)):
+    Y_test[i] +=1
+    
+
+feature_columns = [tf.contrib.layers.real_valued_column("", dimension=9)]
+
+# build 3 layer DNN with 10, 20, 10 units respectively.
+mdir = "/tmp/model"
+classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
+                                              hidden_units=[10,20,10], 
+                                              n_classes=3,
+                                            model_dir=mdir)
+
+def get_train_inputs():
+    x = tf.constant(X_training)
+    y = tf.constant(Y_training)
+    return x, y
+
+# DO THE TRAINING!
+classifier.fit(input_fn=get_train_inputs, steps=2000)
+
+# prep test inputs
+def get_test_inputs():
+    x = tf.constant(X_test)
+    y = tf.constant(Y_test)
+    return x, y
+
+# how good is the training?
+accuracy_score = classifier.evaluate(input_fn=get_test_inputs,
+                                       steps=1)["accuracy"]
+print("\nAccuracy: {0:f}\n".format(accuracy_score))
---
+++ phishing_KNN.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import train_test_split
+
+# Data sets
+PHISHING_DATASET = "phishing_dataset.csv"
+
+# try K=1 through K=25 and record testing accuracy
+k_range = range(1, 26)
+
+# We can create Python dictionary using [] or dict()
+scores = []
+
+# load data
+phishing_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+        filename=PHISHING_DATASET,
+        target_dtype=np.int,
+        features_dtype=np.int)
+
+# prep phishing_dataset inputs
+X_training, X_test, Y_training, Y_test = train_test_split(
+            phishing_set.data,
+            phishing_set.target,
+            test_size=0.4,
+            random_state=4)
+
+# DO THE TRAINING!
+# We use a loop through the range 1 to 26
+# We append the scores in the dictionary
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_training, Y_training)
+    y_pred = knn.predict(X_test)
+    scores.append(metrics.accuracy_score(Y_test, y_pred))
+
+print(scores)
+
+# plot the relationship between K and testing accuracy
+# plt.plot(x_axis, y_axis)
+plt.plot(k_range, scores)
+plt.xlabel('Value of K for KNN')
+plt.ylabel('Testing Accuracy')
+plt.show()
+
